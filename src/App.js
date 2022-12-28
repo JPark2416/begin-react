1c18a3c575ad751f60bbe6620680440b52162a6a
JPark2416 <115630125+JPark2416@users.noreply.github.com>
2022-12-29 04:32:54 +1300

    upto 11

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import Hello from './Hello';
-import './App.css';
+import Counter from './Counter';
+import InputSample from './InputSample';
+import UserList from './UserList';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor : 'black',
-    color : 'aqua',
-    fontSize : 24, 
-    padding: '1rem'
-  }
 
   return (
     <div>
-      <Hello name="react" color="red" />
-      <div style={ style }>{ name }</div>
-      <div className="gray-box"></div>
+      <Counter />
+      <br/>
+      <InputSample />
+      <br/>
+      <UserList />
     </div>
   );
 }
--- src/Hello.js
+++ src/Hello.js
@@ -4,4 +4,8 @@ function Hello({ name, color }) {
   return <div style={{color}}>안녕하세요 {name}</div>
 }
 
+Hello.defaultProps = {
+  name: 'No Name'
+}
+
 export default Hello;
