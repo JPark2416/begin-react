8534e421c1ed5e0b5906ed68ae04181bbf8925fb
JinyoungPark <uc.jinyoungpark@gmail.com>
2023-05-22 23:12:37 +1200

    change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Counter from './Counter';
-import InputSample from './InputSample';
 import UserList from './UserList';
 
 function App() {
 
   return (
     <div>
-      <Counter />
-      <br/>
-      <InputSample />
-      <br/>
       <UserList />
     </div>
   );
