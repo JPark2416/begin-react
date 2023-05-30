ccaeda9bedb0da4411ebd6c7f7ca2abfb0d13af1
JinyoungPark <uc.jinyoungpark@gmail.com>
2023-05-30 22:34:37 +1200

    tab

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./style/Profile.scss";
+import { Tab } from "@headlessui/react";
 import { cv } from "../Model/cv";
 // import userEvent from "@testing-library/user-event";
 
@@ -38,6 +39,19 @@ const Profile = () => {
           ))}
         </div>
       ))}
+
+      <Tab.Group>
+        <Tab.List>
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+          <Tab>Tab 3</Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>Content 1</Tab.Panel>
+          <Tab.Panel>Content 2</Tab.Panel>
+          <Tab.Panel>Content 3</Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
